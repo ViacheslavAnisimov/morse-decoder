b51b92e2d14530995b35ff99b7a806f09af97ec3
ViacheslavAnisimov <forvsn@gmail.com>
2023-08-09 03:36:52 +0700

    feat:solved

--- src/index.js
+++ src/index.js
@@ -39,6 +39,20 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = expr
+    .match(/.{1,10}/g)
+    .map(a => +a)
+    .toString()
+    .replace(/10/g, ".")
+    .replace(/11/g, "-")
+    .split(",")
+    .map(character => MORSE_TABLE[character])
+    .join(" ")
+    .split("  ")
+    .map(a => a.replace(/\s/g, ""))
+    .join(" ")
+
+    return arr;
 }
 
 module.exports = {
